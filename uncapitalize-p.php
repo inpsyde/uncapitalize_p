da8c3ba1e34f574f04374c2e94494c2223a36bb6
Frank Bültge <frank@bueltge.de>
2021-02-27 22:11:51 +0100

    Add filter hook for widgets

--- uncapitalize-p.php
+++ uncapitalize-p.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 /**
  * Plugin Name: Uncapitalize P
- * Description: Gives you the freedom to write «WordPress» as <em>you</em> want to.
+ * Description: Gives you the freedom to write «WordPress» as <em>you</em> want to. Disable Capital P Dangit.
  * Author:      Inpsyde GmbH
- * Author URI:  http://inpsyde.com
- * Version:     2.0.0
+ * Author URI:  https://inpsyde.com
+ * Version:     2.0.1
  * Licence:     MIT
  */
 
@@ -20,6 +20,7 @@ add_action(
             'the_title' => 11,
             'the_content' => 11,
             'comment_text' => 31,
+            'widget_text_content' => 11,
         ];
         foreach ($filters as $filter => $priority) {
             remove_filter($filter, 'capital_P_dangit', $priority);
